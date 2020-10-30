315b2f8b1de23e637f687b340e28cb9904649b91
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2020-10-30 22:32:11 +0200

    Testit korjattu.

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -36,7 +36,7 @@ public class Varasto {
 
     // --- ottavat aksessorit eli getterit: ---
     public double getSaldo() {
-        return 1000;
+        return saldo;
     }
 
     public double getTilavuus() {
